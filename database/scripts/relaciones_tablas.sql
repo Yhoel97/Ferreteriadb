ec2721d021e2d92d9a3635734e07fe182a47c068
Yhoel97 <ra16004@ues.edu.sv>
2026-06-02 02:44:24 -0600

    Creacion de relaciones y claves foraneas

--- database/scripts/relaciones_tablas.sql
+++ database/scripts/relaciones_tablas.sql
@@ -143,3 +143,18 @@ ALTER TABLE Movimiento_Inventario
 ADD CONSTRAINT fk_movimiento_venta
 FOREIGN KEY (id_venta)
 REFERENCES Venta(id_venta);
+
+-- Prueba de las relaciones
+
+SELECT
+    tc.table_name,
+    kcu.column_name,
+    ccu.table_name AS referenced_table,
+    ccu.column_name AS referenced_column
+FROM information_schema.table_constraints AS tc
+JOIN information_schema.key_column_usage AS kcu
+    ON tc.constraint_name = kcu.constraint_name
+JOIN information_schema.constraint_column_usage AS ccu
+    ON ccu.constraint_name = tc.constraint_name
+WHERE tc.constraint_type = 'FOREIGN KEY'
+ORDER BY tc.table_name;
